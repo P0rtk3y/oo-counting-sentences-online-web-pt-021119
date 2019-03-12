31d7aa8c2554b45f3a718f829e0c03e9a5827c0f
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-12 05:32:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.split("."||"?"||"!").delete_if{|e| e == ""||nil}.count 
+    self.split("."||"?"||"!").delete_if{|e| e == ""}.count 
   end
 end
